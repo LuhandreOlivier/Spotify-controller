3df3c1929943cbabd22e25ff66c4d70877149854
Luhandre <155522267+LuhandreOlivier@users.noreply.github.com>
2026-05-10 22:06:17 +0200

    Delete code directory

--- code/secrets.py
+++
@@ -1,9 +0,0 @@
-# secrets.py
-WIFI_SSID = 'Enter you wifi name'
-WIFI_PASSWORD = 'Enter you wifi password'
-
-CLIENT_ID = "Enter you Spotify client ID"
-CLIENT_SECRET = "Enter you Spotify client secret"
-
-ACCESS_TOKEN = 'Enter you Spotify access token'
-REFRESH_TOKEN = 'Enter you Spotify access refresh token'
